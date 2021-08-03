7739b27659b10378cc4b29434385ffd95bb3e9a1
TONGHENG <tonghengit@gmail.com>
2021-08-03 21:35:42 +0700

    hi

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-fgh
+<?php
+ $txt2 = "Learn PHP";
+ $txt2 = "rean24.com";
+ $x = 5;
+ $y =4;
+
+ echo "<h2>" . $txt2 ."</h2>" ;
+ echo "study PHP at " . $txt2 . "<br>";
+ echo "Sum: " . ($x + $y);
+
+?>
+
+<?php 
+   define("name", " Welcom to rean24");
+   echo name;
+?>
