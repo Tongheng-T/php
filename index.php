bbc9079c0dadf9d62ee4926424dabe01b4284f3e
TONGHENG <tonghengit@gmail.com>
2021-08-08 18:58:45 +0700

    p

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 
 
 <?php 
-   $txt = "W3Schools.com";
+   /*$txt = "W3Schools.com";
    echo "I love $txt!";
    echo "<br>";
    echo "I love " . $txt . "!";
@@ -23,4 +23,50 @@
    echo "<br>";
 
    echo $x + $y;
+
+   $x = 5; // global scope
+
+function myTest() {
+  // using x inside this function will generate an error
+  echo "<p>Variable x inside function is: </p>";
+}
+myTest();
+
+echo "<p>Variable x outside function is: $x</p>";*/
+echo "<center>Hello PHP </center>";
+echo "<p><font color=blue>One lin simple string in blue color </font></p>";
+echo "<p><font color=#ff65>One lin simple string in blue color </font></p>";
+echo "<p><font color=#04aa6d>One lin simple string in blue color </font></p>";
+
+$a=1000;
+$b=1200;
+$c=1400;
+echo "<p> <fomt color=blue size='4pt'> Salary of Mr.A is:</fomt> <font color=red size='4pt'>$a$</font></p>";
+echo "<p> <fomt color=blue size='4pt'> Salary of Mr.b is:"."</fomt> <font color=red size='4pt'>$b$</font></p>";
+echo "<p> <fomt color=blue size='4pt'> Salary of Mr.c is:"."</fomt> <font color=red size='4pt'>$c$</font></p>";
+
+echo "<table border=1 cellspacing=0 cellpading=0> Monthly Salary Statement</table>";
+
+echo "<table border=1 cellspacing=0 cellpading=0>
+<tr> 
+<td>
+<font color=blue> Salary of Mr. A is</font> 
+</td>
+<td>$a$</td>
+</tr>
+
+<tr> <td><font color=blue> Salary of Mr. b is</td> <td>$b$</font></td></tr>
+<tr> <td><font color=blue> Salary of Mr. c is</td> <td>$c$</font></td></tr>
+</table>";
+
+echo "<ul align='left'> <font color=red size='4pt'> 
+Salary statement for the Current month</font> 
+<li> <font color=blue> Salary of Mr.A is :$a$ </font></li>
+<li> <font color=blue> Salary of Mr.b is :$b$ </font></li>
+<li> <font color=blue> Salary of Mr.c is :$c$ </font></li>
+
+</ul> ";
+
+
 ?>
+Save: Demo2.php
