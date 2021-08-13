b1483b980bfe4eb0f6e2b05a7a5c02b41245bdda
TONGHENG <tonghengit@gmail.com>
2021-08-13 19:52:19 +0700

    h

--- index.php
+++ index.php
@@ -163,3 +163,58 @@ if ($x === $y){
 }
 //echo $x+$y;
 ?>
+
+<?php 
+$diceNumber = 4;
+if ($diceNumber == 1){
+    echo "The dice number is 1";
+}else if ($diceNumber % 2 == 0) {
+    if ($diceNumber == 2){
+        echo "The dice number is 2";
+    }elseif ($diceNumber == 4) {
+        echo "The dice number is 4";
+    }elseif ($diceNumber == 6) {
+        echo "The dice number is 6";
+    }else {
+        echo "The number has to be between 1 and 6";
+    }
+} elseif ($diceNumber == 3) {
+    echo "The dice number is 3";
+}elseif ($diceNumber == 5) {
+    echo "The dice number is 5";
+}else {
+    echo "The number has to be between 1 and 6";
+}
+echo "<br>";
+
+?>
+
+
+
+<?php 
+$name= 'sa';
+$pass='123';
+$cpass='123';
+$utype= 'admin';
+ if (strcmp($name,'sa')==0)
+     if(strcmp($pass,'123')==0)
+      if(strcmp($cpass,'123')==0)
+       if(strcmp($utype,'admin')==0)
+         echo "Welcome to log in";
+           else 
+              echo "Invalid User Type!";
+        else 
+          echo "Invalid Confirm Password";
+      else 
+        echo "Invalid Password";
+else 
+  echo "Invalid User Name";
+ 
+
+?>
+
+<?php 
+
+
+?>
+​
