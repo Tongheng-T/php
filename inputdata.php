fe7bb1468a70ab6ba917460aa3b11706f2134624
Tongheng-T <87926827+Tongheng-T@users.noreply.github.com>
2021-08-23 20:39:23 +0700

    Update inputdata.php
    update

--- inputdata.php
+++ inputdata.php
@@ -15,44 +15,39 @@
     </style>
 
 <div id="center">
-    <h2>  Forms Input  </h2>
+    <h2>  Forms Input </h2>
 
-<form method="post">
+<form method="POST">
 
-   <p>ID: <input type="number" name="id" value="00" autofocus ></p>
+   <p>ID: <input type="number" name="id" value="00" required ></p>
 
-    <p> Name: <input type="text" name="name" value="" autofocus ></p>
-
-   <p>Gender:  Male: <input type="radio" name="sex" value="Male" checked= "checked">
+    <p> Name: <input type="text" name="name" value="" required ></p>
+  
+   <p>Gender: Male: <input type="radio" name="sex" value="Male" checked= "checked">
    
    Female: <input type="radio" name="sex" value="Female" checked= "checked"></p>
 
-   <p>Salary: <input type="text" name="salary" value=""></p>
+   <p>Salary: <input type="text" name="salary" value=""required></p>
 
    <input type="submit" name="submit" value="Submit" />
 
-   &nbsp;&nbsp;&nbsp;
-
-   <input type="submit" name="clear" value="Clear" />
-
 </form>
 <?php
-    $display="";
-    $id = $_POST['id'];
-    $name = $_POST['name'];
-    $sex = $_POST['sex'];
-    $salary = $_POST['salary'];
 
     if(isset($_POST['submit'])){
+      $display="";
+      $id = $_POST['id'];
+      $name = $_POST['name'];
+      $sex = $_POST['sex'];
+      $salary = $_POST['salary'];
+
         $display .= "id: ".($id) ."<br>";
         $display .= "name: ".($name) ."<br>";
         $display .= "sex: ".($sex) ."<br>";
         $display .= "salary: ".($salary) ."<br>"; 
 
         if($sex=="Female"  && $salary >= 100){
-            $dis = 20/100;
-           
-           
+            $dis = 20/100;   
         }elseif($sex=="Male" && $salary >= 100)  {
           $dis = 15/100; 
         }else {
@@ -64,18 +59,6 @@
         echo "bonus: ".$bonus;
         echo $pay;
     }
-    if (isset($_POST['clear'])) {
-
-        $firstname ="";
-     
-        $middlename ="";
-     
-        $lastname ="";
-     
-        $display ="";
-     
-        } 
-     
    ?> 
 </div>
  </body>
